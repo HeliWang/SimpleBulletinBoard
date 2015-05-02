b8e2883c252acd2f7e55be2ca24d693391168958
Heli Wang <h379wang@uwaterloo.ca>
2015-05-02 10:40:42 -0400

    delete unused file tables.php

--- tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "mysql.php";
-
-$table = "entries";
-mysql_query ("CREATE TABLE IF NOT EXISTS $table (ID INT NOT NULL)");
-mysql_query ("ALTER TABLE $table ADD title TEXT NOT NULL");
-mysql_query ("ALTER TABLE $table ADD summary TEXT NOT NULL");
-mysql_query ("ALTER TABLE $table ADD content TEXT NOT NULL");
-?>
